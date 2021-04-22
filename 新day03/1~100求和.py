a8ade3d52e50cb46f2a33f8652c31b985d7b893c
“GOng021 <529217388@qq.com>
2021-04-22 20:00:34 +0800

    第三天

---
+++ 新day03/1~100求和.py
@@ -0,0 +1,7 @@
+num =0
+sum =0
+while num <=100:
+    sum = sum +num
+    num = num +1
+
+print(sum)
---
+++ 新day03/青蛙.py
@@ -0,0 +1,21 @@
+height=0
+day=0
+while height<20:
+     height = height +3
+     if height<20:
+        height=height-2
+     day = day + 1
+print("需要天数:",day)
+
+
+
+
+
+
+
+
+
+
+
+
+
